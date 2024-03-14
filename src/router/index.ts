c6acfd19220e2b66617888e18671dc156dcd0582
varenraven <micradtke20@gmail.com>
2024-03-14 19:16:47 +0100

    dodano obsłużenie widoku notatnika

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,7 +32,7 @@ const router = createRouter({
     // route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/ProfilView.vue')
+    component: () => import('../views/ProfileView.vue')
     },
     {
       path: '/podopieczni',
@@ -50,6 +50,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TutorialsView.vue')
     },
+
     {
       path: '/logowanie',
       name: 'logowanie',
@@ -65,8 +66,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: '/notatki-użytkownika',
+      name: 'notatki użytkownika',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/NotesView.vue')
     }
-
   ]
 })
 
