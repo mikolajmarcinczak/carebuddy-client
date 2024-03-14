d5d2b08ac0e929adf2a155eed34a5864c206db7d
varenraven <micradtke20@gmail.com>
2024-03-14 19:10:29 +0100

    Przycisk zaloguj się (w formularzu rejestracji) odsyła do widoku logowania

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -33,7 +33,7 @@
             </div>
             <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Utwórz konto</button>
             <p class="text-xl font-light text-white- dark:text-white">
-              Posiadasz już konto? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Zaloguj się</a>
+              Posiadasz już konto? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500"><router-link to="/logowanie">Zaloguj się</router-link></a>
             </p>
           </form>
         </div>
