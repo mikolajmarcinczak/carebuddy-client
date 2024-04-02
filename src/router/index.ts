f8847c6228a18b95d15483fb61187846ac9e14d3
varenraven <micradtke20@gmail.com>
2024-04-02 16:48:45 +0200

    Notatnik. Zmiany w obrębie notatnika i jego komponentów

--- src/router/index.ts
+++ src/router/index.ts
@@ -76,14 +76,6 @@ const router = createRouter({
       component: () => import('../views/NotesView.vue')
     },
     {
-      path: '/dodawanie-notatki',
-      name: 'nowa notatka',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AddNewNoteView.vue')
-    },
-    {
       path: '/kalendarz',
       name: 'kalendarz',
       // route level code-splitting
